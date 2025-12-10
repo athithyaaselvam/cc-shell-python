3f43ff3329a87f214b43d2a34c3b5cef81023b5b
Athithyaa Selvam <33496652+athithyaaselvam@users.noreply.github.com>
2025-12-10 11:47:36 -0800

    Implement type command

--- app/main.py
+++ app/main.py
@@ -11,11 +11,17 @@ def main():
         cmd = parts[0]
         if cmd == "exit":
             break
-        if cmd == "echo":
-            if len(parts[1]) > 1:
+        elif cmd == "echo":
+            if len(parts) > 1:
                 print(parts[1])
             else:
-                print("") 
+                print("")
+        elif cmd == "type":
+            if len(parts) <=2 :
+                if parts[1] in ("echo", "exit", "type"):
+                    print(f"{parts[1]} is a shell builtin")
+                else:
+                    print(f"{parts[1]}: not found")
         else:
             print(f"{command}: command not found")
     pass
