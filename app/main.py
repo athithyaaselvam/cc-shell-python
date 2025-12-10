94d84eb79df1d94d5f6b529ff314ce2a72bad585
Athithyaa Selvam <33496652+athithyaaselvam@users.noreply.github.com>
2025-12-10 11:38:49 -0800

    Implement echo command

--- app/main.py
+++ app/main.py
@@ -6,10 +6,18 @@ def main():
     # Wait for user input
     while True:
         sys.stdout.write("$ ")
-        command = input()
-        if command == "exit":
+        command = input().strip()
+        parts = command.split(maxsplit=1)
+        cmd = parts[0]
+        if cmd == "exit":
             break
-        print(f"{command}: command not found")
+        if cmd == "echo":
+            if len(parts[1]) > 1:
+                print(parts[1])
+            else:
+                print("") 
+        else:
+            print(f"{command}: command not found")
     pass
 
 
