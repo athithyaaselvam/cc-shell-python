45687039f6fd455128071b3ff75af48bcc90210d
Athithyaa Selvam <33496652+athithyaaselvam@users.noreply.github.com>
2025-12-09 21:35:35 -0800

    Implement a REPL

--- app/main.py
+++ app/main.py
@@ -3,11 +3,13 @@ import sys
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
-    sys.stdout.write("$ ")
+    
     
     # Wait for user input
-    command = input()
-    print(f"{command}: command not found")
+    while True:
+        sys.stdout.write("$ ")
+        command = input()
+        print(f"{command}: command not found")
     pass
 
 
