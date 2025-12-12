d2b15c01056a4efae77e39a0a838b65a8460d9e2
Athithyaa Selvam <33496652+athithyaaselvam@users.noreply.github.com>
2025-12-11 20:19:01 -0800

    Locate executable files

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import sys
-
+import os
 
 def main():
 
@@ -9,6 +9,10 @@ def main():
         command = input().strip()
         parts = command.split(maxsplit=1)
         cmd = parts[0]
+        path = os.getenv("PATH", "")    #returned as string
+        dirs = path.split(os.pathsep)
+        found_executable = False
+
         if cmd == "exit":
             break
         elif cmd == "echo":
@@ -17,10 +21,21 @@ def main():
             else:
                 print("")
         elif cmd == "type":
-            if len(parts) <=2 :
+            if len(parts) ==2 :
                 if parts[1] in ("echo", "exit", "type"):
                     print(f"{parts[1]} is a shell builtin")
-                else:
+                    continue
+                
+                #there could be multiple path elements.
+                #for each element we need to check each dir
+                for directory in dirs:
+                    full_file_path = os.path.join(directory, parts[1])
+                    if os.path.isfile(full_file_path) and os.access(full_file_path, os.X_OK):
+                        print(f"{parts[1]} is {full_file_path}")  
+                        found_executable = True
+                        break
+                    
+                if not found_executable:
                     print(f"{parts[1]}: not found")
         else:
             print(f"{command}: command not found")
